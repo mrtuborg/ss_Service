2547acee2791d5c9d18d33d7da3f044033f11465
Vladimir A. Nosenko <nosenko@nicetu.spb.ru>
2011-01-26 13:17:34 +0300

    новые файлы в соответствии с новой моделью сборки

---
+++ services/0/functions.h
@@ -0,0 +1,19 @@
+// Selfdiagnostics system service
+//--------------------------------
+
+extern errType equipListenProcessing(BYTE *writingBuffer, size_t sz);
+
+extern errType srvInit();
+extern errType srvDeinit();
+
+extern errType emergencyShutdown(void*);
+extern errType controlModeChange(void*);
+extern errType getStateVector(void*);
+
+extern errType GetProcessesList(void*);
+extern errType GetProcessInfo(void*);
+extern errType RebootSystem(void*);
+extern errType SearchProcess(void*);
+extern errType KillProcess(void*);
+extern errType RestartProcess(void*);
+extern errType InstallFilter(void*);
